4eafeb681f5934d8932b774ec22b8ae11ed6383f
nosrac77 <carson.newton@outlook.com>
2017-08-12 16:14:25 -0700

    completed all TODO tasks for lab 7!

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,17 +7,15 @@
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.listen(3000)
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
-app.use('/public');
+app.use(express.static('./public'));
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -25,7 +23,7 @@ app.post('/articles', bodyParser, function(request, response) {
   // write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
-})
+});
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
